8d031133d43ae2d2ce319caedcbd70558425754a
xabdev <xab.dev@gmail.com>
2023-02-23 19:32:36 -0300

    added curl to get info of the song

--- get_song.sh
+++ get_song.sh
@@ -4,6 +4,7 @@ song_url=""
 song_url_arr=()
 
 
+#loop through file.txt and add each line to song_url_arr array
 while read line
 do
     song_url_arr+=($line) 
@@ -18,8 +19,12 @@ echo "display number of items in array"
 echo $songs
 
 
-
-
+#loop through array to process yt-dlp commands
 for ((i = 0; i < $songs; i++)); do
-    yt-dlp -f 'ba' -x --audio-format mp3 -o "songs/%(title)s.%(ext)s" ${song_url_arr[i]}
+    echo ""
+    echo "> Downloading: " 
+    curl -s ${song_url_arr[i]} | perl -MHTML::Entities -ne 'binmode(STDOUT, ":utf8");if(/<title>([^<]*) - YouTube<\/title>/){print decode_entities($1),"\n"}'
+    yt-dlp -q --progress --newline -f 'ba' -x --audio-format m4a -o "songs/%(title)s.%(ext)s" ${song_url_arr[i]}
+    echo "> done!"
+    echo ""
     done
