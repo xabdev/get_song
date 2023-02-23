990e17006b885dcf5a676d1147e27b197cb13fe5
xabdev <xab.dev@gmail.com>
2023-02-23 20:31:12 -0300

    finished!

--- get_song.sh
+++ get_song.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-song_url=""
+
+#check if dependencies are installed
+if ! command -v curl &> /dev/null; then
+    echo "curl is not installed. Please install it before running this script."
+    exit 1
+fi
+
+if ! command -v perl &> /dev/null; then
+    echo "perl is not installed. Please install it before running this script."
+    exit 1
+fi
+
+if ! command -v yt-dlp &> /dev/null; then
+    echo "yt-dlp is not installed. Please install it before running this script."
+    exit 1
+fi
+
+
+
+
 song_url_arr=()
 
 
@@ -13,18 +32,24 @@ done < file.txt
 
 songs=${#song_url_arr[@]}
 
-echo "Number of songs: $songs"
 
-echo "display number of items in array"
-echo $songs
 
+echo "This songs will be downloaded: "
+echo ""
+
+
+#loop and output the video title
+for ((i = 0; i < $songs; i++)); do
+    curl -s ${song_url_arr[i]} | perl -MHTML::Entities -ne 'binmode(STDOUT, ":utf8");if(/<title>([^<]*) - YouTube<\/title>/){print decode_entities($1),"\n"}'
+    done
+
+echo ""
 
 #loop through array to process yt-dlp commands
 for ((i = 0; i < $songs; i++)); do
-    echo ""
     echo "> Downloading: " 
     curl -s ${song_url_arr[i]} | perl -MHTML::Entities -ne 'binmode(STDOUT, ":utf8");if(/<title>([^<]*) - YouTube<\/title>/){print decode_entities($1),"\n"}'
-    yt-dlp -q --progress --newline -f 'ba' -x --audio-format m4a -o "songs/%(title)s.%(ext)s" ${song_url_arr[i]}
+    yt-dlp -q --progress -f 'ba' --extract-audio  -o "songs/%(title)s.%(ext)s" ${song_url_arr[i]}
     echo "> done!"
     echo ""
     done
